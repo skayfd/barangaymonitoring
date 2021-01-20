fc9261f8ded384ba05e69c40f9b8902143debe64
Happibee <xkendaru@gmail.com>
2021-01-20 18:06:57 +0800

    Changed DB Address to 000webhost mysql

--- config/database.php
+++ config/database.php
@@ -7,10 +7,10 @@
 	// private $password = "";
 
 	//Deployment Connection
-	private	$host = "remotemysql.com";//from remotemysql.com
-	private $dbName = "iluUDmXZz2";
-	private	$userName = "iluUDmXZz2";
-	private $password = "sd1Flri7PH";
+	// private	$host = "remotemysql.com";//from remotemysql.com
+	// private $dbName = "iluUDmXZz2";
+	// private	$userName = "iluUDmXZz2";
+	// private $password = "sd1Flri7PH";
 
 	//Deployment Connection for 000webhosting
 	private	$host = "localhost";//from remotemysql.com
